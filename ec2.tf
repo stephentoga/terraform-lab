a6f89f683a732d48c61a19a25eecec4522105a6b
stephentoga <chukwuemeka_richard@yahoo.com>
2020-02-16 17:54:32 -0600

    ignoring terraform plan files

--- ec2.tf
+++ ec2.tf
@@ -3,22 +3,22 @@ provider "aws" {
 }
 
 resource "aws_instance" "patrollie-ec2" {
-  ami           = "ami-09f2114fecbe506e2"
+  ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
- key_name = "patronelli"
-  security_groups = ["${aws_security_group.allow_rdp-2-patronelli.name}"]
+  key_name = "patronelli"
+  security_groups = [aws_security_group.allow_rdp-2-patronelli.name]
 
 }
 
 resource "aws_security_group" "allow_rdp-2-patronelli" {
-  name        = "allow_rdp-2-patronelli"
-  description = "Allow rdp traffic"
+  name        = "allow_ssh-2-patronelli"
+  description = "Allow ssh traffic"
 
 
   ingress {
 
-    from_port   = 3389 
-    to_port     = 3389
+    from_port   = 22 
+    to_port     = 22
     protocol =   "tcp"
 
     cidr_blocks =  ["0.0.0.0/0"]
